22c74fc45f72e6caf44f8bc34ba2e924ec44ac52
ray crowley <raymecrowley@gmail.com>
2019-03-21 11:13:41 +0000

    add recipes.html and style.css

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 import os
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'my_cookbook'
+app.config["MONGO_URI"] = 'mongodb+srv://root:R00t75@cluster0-efmd8.mongodb.net/my_cookbook?retryWrites=true'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def my_cookbook():
-    return 'My Cookbook'
+@app.route('/list_recipes')
+def list_recipes():
+    return render_template('recipes.html', recipes = mongo.db.recipes.find())
     
 if __name__ == '__main__':
     
