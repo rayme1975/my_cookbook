b56682155cd1f8a6190a60f069d42943d716fa07
ray crowley <raymecrowley@gmail.com>
2019-04-10 19:14:41 +0000

    updated app.py file with full CRUD functionality

--- app.py
+++ app.py
@@ -11,10 +11,95 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:R00t75@cluster0-efmd8.mongodb.net/
 
 mongo = PyMongo(app)
 
+#home page
 @app.route('/')
+@app.route('/home_page')
+def home_page():
+    
+    return render_template('home.html', categories = mongo.db.categories.find())
+    
+#show full list of recipes
 @app.route('/list_recipes')
 def list_recipes():
-    return render_template('recipes.html', recipes = mongo.db.recipes.find())
+    
+    return render_template('list_recipes.html', recipes = mongo.db.recipes.find(), categories = mongo.db.categories.find())
+
+
+#show an individual recipe
+@app.route('/show_recipe/<recipe_id>')
+def show_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('recipes.html', recipe = the_recipe, categories = all_categories)
+    
+
+
+# show favourites
+@app.route('/show_favourites')
+def show_favourites():
+    return render_template('favourites.html', recipe = mongo.db.recipes.find(), categories = mongo.db.categories.find(), favourites = mongo.db.recipes.find( {"add_favourite": "on" } ))
+                                                                                                                                                                
+                                                                                                                                    
+    
+#add a recipe 
+@app.route('/add_recipe')
+def add_recipe():
+    _categories = mongo.db.categories.find()
+    category_list = [category for category in _categories]
+    
+    return render_template('add_recipe.html', categories = category_list)
+
+#insert recipe into mongoDB that you added with #add a recipe
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('list_recipes'))
+
+#edit a recipe    
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    category_list = [category for category in all_categories]
+    return render_template('editrecipe.html', recipe = the_recipe, categories = category_list)
+    
+# delete an individual recipe
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    remove_recipe = mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return redirect(url_for('list_recipes', recipe = remove_recipe, categories = all_categories))
+    
+#update database
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipe_updated = mongo.db.recipes
+    recipe_updated.update({"_id": ObjectId(recipe_id)},
+    {
+    
+    'category_name':request.form.get('category_name'),
+    'title':request.form.get('title'),
+    'description':request.form.get('description'),
+    'ingredients':request.form.get('ingredients'),
+    'preparation_time':request.form.get('preparation_time'),
+    'cooking_time':request.form.get('cooking_time'),
+    'cooking_instructions':request.form.get('cooking_instructions'),
+    'vegan_friendly':request.form.get('vegan_friendly'),
+    'gluten_free':request.form.get('gluten_free'),
+    'add_favourite':request.form.get('add_favourite'),
+    'difficulty':request.form.get('difficulty')
+    
+    
+    })
+    return redirect(url_for('list_recipes'))
+    
+    
+    
+    
+
+
+    
     
 if __name__ == '__main__':
     
