a9a30fcf8d846df195c6d68e550b9109b71c7df0
ray crowley <raymecrowley@gmail.com>
2019-04-30 16:11:21 +0000

    style ingredients and cooking_instructions

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def update_recipe(recipe_id):
     
 
 if __name__ == '__main__':
+        app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),debug=True)
     
-    app.run(host=os.environ.get('IP'),port=int(os.environ.get('PORT')),
-    debug=True)
+    
 
